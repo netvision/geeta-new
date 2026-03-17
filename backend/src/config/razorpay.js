369124b5201b15b04be12c63ae5b72fc7869497a
Rakesh Jangid <rakesh@jangid.co.in>
2026-03-17 08:36:38 +0530

    Fix: skip Razorpay init when credentials not set

--- backend/src/config/razorpay.js
+++ backend/src/config/razorpay.js
@@ -1,8 +1,10 @@
 const Razorpay = require('razorpay');
 
-const razorpay = new Razorpay({
-  key_id: process.env.RAZORPAY_KEY_ID,
-  key_secret: process.env.RAZORPAY_KEY_SECRET,
-});
+const razorpay = process.env.RAZORPAY_KEY_ID
+  ? new Razorpay({
+      key_id: process.env.RAZORPAY_KEY_ID,
+      key_secret: process.env.RAZORPAY_KEY_SECRET,
+    })
+  : null;
 
 module.exports = razorpay;
